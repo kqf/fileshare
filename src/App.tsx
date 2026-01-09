53c7e21b048288f7af25ad32c03228d150c43ad7
kqf <q.dev.null@gmail.com>
2026-01-09 07:07:02 +0100

    Simplify the view (#27)
    * Simplify
    
    * Simplify
    
    * fix
    
    * Clean the alerting up
    
    * Remove duplicate server
    
    * Drop the redundant error messages
    
    * Drop the redundant error messages
    
    * Simplify the view

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ function sendClientContext() {
     timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
     language: navigator.language
   }
-
   navigator.sendBeacon('/_context', JSON.stringify(payload))
 }
 
@@ -25,9 +24,6 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
