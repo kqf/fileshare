a3f2733ae6a635e04f51c0c23cb4b80d0062aefc
kqf <q.dev.null@gmail.com>
2026-02-08 07:09:49 +0000

    Clean the log messages up

--- alert.py
+++ alert.py
@@ -130,17 +130,15 @@ async def handle_log(log: dict, logger: Logger) -> None:
 
 async def main() -> None:
     logger = build_logger()
-    await logger.notify("Deployed the alerter")
-
-    await logger.notify("starting the alerter")
-    await logger.notify("🟢 waiting for nginx logs...\n")
+    await logger.notify("🟢 Starting the alerter")
+    await logger.notify("🟢 Waiting for nginx logs...\n")
     app = web.Application()
     app.router.add_post("/_context", partial(handle_context, logger=logger))
     app.router.add_post("/_frame", partial(handle_frame, logger=logger))
 
     runner = web.AppRunner(app)
     await runner.setup()
-    await logger.notify("🟢 listening on 127.0.0.1:3001\n")
+    await logger.notify("🟢 Listening on 127.0.0.1:3001\n")
     await web.TCPSite(runner, "127.0.0.1", 3001).start()
     asyncio.create_task(read_file(Path("/var/log/nginx/access.log"), logger))
     await logger.notify("🟢 Healthy\n")
