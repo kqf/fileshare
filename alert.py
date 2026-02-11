745ebab144d4cc90c418fbd8aec546ddb2ebc70c
kqf <q.dev.null@gmail.com>
2026-02-11 07:02:15 +0000

    Fix the ngnix logs again (#91)
    * Fix the alerting
    
    * Drop the file alerting
    
    * Configure the alerter properly
    
    * Don't log to stdout
    
    * Trust all the proxies
    
    * Fix the nginx logs
    
    * Extend the logged part
    
    * Fix the alerter

--- alert.py
+++ alert.py
@@ -90,6 +90,7 @@ async def handle_frame(request: web.Request, logger: Logger) -> web.Response:
 
 async def read_file(path: Path, logger: Logger) -> None:
     async with aiofiles.open(path, "r") as f:
+        await f.seek(0, 2)
         while True:
             line = await f.readline()
             if not line:
