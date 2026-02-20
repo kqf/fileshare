002243e2159b269e5197feed6a0b930e73b4474d
kqf <q.dev.null@gmail.com>
2026-02-20 07:18:08 +0000

    Fix the entrypoints (#110)
    * Fix the broken eslint
    
    * Fix the entrypoints
    
    * Add the missing frame endpoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 set -e
 
-python3 /app/alert.py &
-touch /var/log/nginx/access.log &
-tail -f /var/log/nginx/access.log &
-nginx -g 'daemon off;'
+python3 /app/alert.py & nginx -g 'daemon off;'
